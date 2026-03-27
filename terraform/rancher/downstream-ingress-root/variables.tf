a57da559d649c6b393ca08eb510200227e1d9996
Pascal Garcia <garcia.pass@gmail.com>
2026-03-27 17:29:58 +0100

    feat: create downstream node IAM role and instance profile in terraform

--- terraform/rancher/downstream-ingress-root/variables.tf
+++ terraform/rancher/downstream-ingress-root/variables.tf
@@ -1,8 +1,3 @@
-variable "downstream_node_instance_profile_name" {
-  type        = string
-  description = "Existing IAM instance profile name attached to downstream RKE2 EC2 nodes."
-}
-
 variable "aws_load_balancer_controller_chart_version" {
   type        = string
   description = "Helm chart version for aws-load-balancer-controller."
