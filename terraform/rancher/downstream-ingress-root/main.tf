4615f32bc9a390e42e6f0b97b71c77e67f997477
Pascal Garcia <garcia.pass@gmail.com>
2026-04-23 12:43:09 +0200

    Remove unused aws-load-balancer-controller from downstream ingress

--- terraform/rancher/downstream-ingress-root/main.tf
+++ terraform/rancher/downstream-ingress-root/main.tf
@@ -1,37 +1,5 @@
-resource "helm_release" "aws_load_balancer_controller" {
-  repository       = "https://aws.github.io/eks-charts"
-  chart            = "aws-load-balancer-controller"
-  name             = "aws-load-balancer-controller"
-  namespace        = "kube-system"
-  create_namespace = false
-  version          = var.aws_load_balancer_controller_chart_version
-  atomic           = true
-  cleanup_on_fail  = true
-  wait             = true
-  timeout          = 600
-
-  set = [
-    {
-      name  = "clusterName"
-      value = data.terraform_remote_state.downstream_rke2.outputs.cluster_name
-    },
-    {
-      name  = "region"
-      value = data.terraform_remote_state.downstream_rke2.outputs.aws_region
-    },
-    {
-      name  = "vpcId"
-      value = data.terraform_remote_state.downstream_rke2.outputs.aws_vpc_id
-    },
-    {
-      name  = "serviceAccount.create"
-      value = "true"
-    },
-  ]
-}
 
 resource "kubernetes_manifest" "rke2_traefik_config" {
-  depends_on = [helm_release.aws_load_balancer_controller]
 
   manifest = {
     apiVersion = "helm.cattle.io/v1"
